01c8190062454669876c2bd0856d900680f07816
Wahid <wahid@gmail.com>
2026-05-02 21:01:42 +0600

    POS page updated searchbar to datatables searchbar and auto addto cart from barcode scanner, editable qty in cart, remove button in cart

--- POS/POS.aspx
+++ POS/POS.aspx
@@ -1,39 +1,44 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="POS.aspx.cs" Inherits="POS.POS" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+    <!-- DataTables -->
+    <%--<link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet" />
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>--%>
+
 </asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <div class="container-fluid py-3">
 
-    <!-- TOP SEARCH BAR -->
+<div class="container-fluid py-3">
+
+    <!-- ✅ CUSTOM SEARCH BAR -->
     <div class="row mb-3">
-        <div class="col-md-8">
-            <div class="input-group">
-                <asp:TextBox ID="txtSearch" runat="server"
-                    CssClass="form-control"
-                    placeholder="Scan barcode / Enter product code / search product..." />
-
-                <asp:Button ID="btnSearch" runat="server"
-                    Text="Search"
-                    CssClass="btn btn-primary"
-                    OnClick="btnSearch_Click" />
-            </div>
+        <div class="col-md-6">
+            <input type="text" id="customSearch"
+                class="form-control"
+                placeholder="Scan barcode / search product..." />
         </div>
     </div>
 
-    <!-- MAIN SECTION -->
     <div class="row g-3">
 
-        <!-- LEFT: PRODUCT LIST -->
+        <!-- LEFT -->
         <div class="col-md-7">
 
             <asp:GridView ID="gvProducts" runat="server"
+                ClientIDMode="Static"
                 CssClass="table table-bordered table-hover"
                 AutoGenerateColumns="False"
+                OnPreRender="gvProducts_PreRender"
                 OnRowCommand="gvProducts_RowCommand">
 
                 <Columns>
-
                     <asp:BoundField DataField="Product_Name" HeaderText="Product" />
+                    <asp:BoundField DataField="Product_Code" HeaderText="Code" />
+                    <asp:BoundField DataField="Barcode_No" HeaderText="Barcode" />
                     <asp:BoundField DataField="Unit_Price" HeaderText="Price" />
                     <asp:BoundField DataField="Stock" HeaderText="Stock" />
 
@@ -46,37 +51,58 @@
                                 CommandArgument='<%# Eval("ser") %>' />
                         </ItemTemplate>
                     </asp:TemplateField>
-
                 </Columns>
 
             </asp:GridView>
 
         </div>
 
-        <!-- RIGHT: BILL PANEL -->
+        <!-- RIGHT -->
         <div class="col-md-5">
 
             <div class="border rounded-4 shadow-sm bg-light p-3">
 
                 <h5 class="text-center fw-bold mb-3">Bill Summary</h5>
 
-                <!-- CART TABLE -->
                 <asp:GridView ID="gvCart" runat="server"
+                    DataKeyNames="ProductId"
                     CssClass="table table-sm table-bordered"
-                    AutoGenerateColumns="False">
+                    AutoGenerateColumns="False"
+                    OnRowCommand="gvCart_RowCommand">
 
                     <Columns>
                         <asp:BoundField DataField="Product_Name" HeaderText="Item" />
-                        <asp:BoundField DataField="Qty" HeaderText="Qty" />
+                        
+                        <asp:TemplateField HeaderText="Qty" ItemStyle-Width="80px" HeaderStyle-Width="80px">
+                            <ItemTemplate>
+                                <asp:TextBox ID="txtQty" runat="server"
+                                    Text='<%# Eval("Qty") %>'
+                                    CssClass="form-control text-center form-control-sm text-end qtyBox"
+                                    AutoPostBack="true"
+                                    OnTextChanged="txtQty_TextChanged"
+                                    CommandArgument='<%# Eval("ProductId") %>'>
+                                </asp:TextBox>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+
                         <asp:BoundField DataField="Price" HeaderText="Price" />
                         <asp:BoundField DataField="Total" HeaderText="Total" />
+
+                        <asp:TemplateField>
+                            <ItemTemplate>
+                                <asp:Button ID="btnRemove" runat="server"
+                                    Text="X"
+                                    CommandName="RemoveItem"
+                                    CommandArgument='<%# Eval("ProductId") %>'
+                                    CssClass="btn btn-danger btn-sm" />
+                            </ItemTemplate>
+                        </asp:TemplateField>
                     </Columns>
 
                 </asp:GridView>
 
                 <hr />
 
-                <!-- TOTALS -->
                 <div class="d-flex justify-content-between">
                     <span>Subtotal:</span>
                     <asp:Label ID="lblSubtotal" runat="server" Text="0.00"></asp:Label>
@@ -85,6 +111,8 @@
                 <div class="d-flex justify-content-between">
                     <span>Discount:</span>
                     <asp:TextBox ID="txtDiscount" runat="server"
+                        AutoPostBack="true"
+                        OnTextChanged="txtDiscount_TextChanged"
                         CssClass="form-control form-control-sm w-50 text-end" Text="0" />
                 </div>
 
@@ -105,6 +133,42 @@
         </div>
 
     </div>
-
 </div>
+
+<!-- ✅ DATATABLES SCRIPT -->
+<script>
+    $(document).ready(function () {
+
+        var table = $('#gvProducts').DataTable({
+            dom: 't', // 🔥 hide default search + pagination UI
+            ordering: false
+        });
+
+        // ✅ connect your custom search box
+        $('#customSearch').on('keyup', function () {
+            table.search(this.value).draw();
+        });
+
+        // auto focus (for barcode scanner)
+        $('#customSearch').focus();
+    });
+
+    //Auto add when barcode scanned
+    $('#customSearch').on('change', function () {
+        var value = this.value.trim();
+
+        if (value.length > 0) {
+            // try auto click first matching row
+            var table = $('#gvProducts').DataTable();
+            table.search(value).draw();
+
+            setTimeout(function () {
+                $('#gvProducts tbody tr:first .btn-success').click();
+            }, 200);
+
+            $('#customSearch').val('').focus();
+        }
+    });
+</script>
+
 </asp:Content>
--- POS/POS.aspx.cs
+++ POS/POS.aspx.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,37 +8,251 @@ namespace POS
 {
     public partial class POS : System.Web.UI.Page
     {
+        string cs = "Data Source=DESKTOP-G0J1RC1\\SQL17;Initial Catalog=POS;Integrated Security=True";
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                LoadProducts();
+                InitCart();
+            }
         }
-        protected void btnSearch_Click(object sender, EventArgs e)
-        {
-            string keyword = txtSearch.Text.Trim();
 
-            // test first
-            System.Diagnostics.Debug.WriteLine("Search clicked: " + keyword);
+        private void InitCart()
+        {
+            if (Session["Cart"] == null)
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.Add("ProductId");
+                dt.Columns.Add("Product_Name");
+                dt.Columns.Add("Qty", typeof(decimal));
+                dt.Columns.Add("Price", typeof(decimal));
+                dt.Columns.Add("Total", typeof(decimal));
+
+                Session["Cart"] = dt;
+            }
+        }
+
+        private void LoadProducts(string keyword = "")
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                string query = "SELECT * FROM Product_TBL";
+
+                if (!string.IsNullOrEmpty(keyword))
+                {
+                    query += @" WHERE 
+                        Product_Name LIKE @key OR 
+                        Product_Code LIKE @key OR 
+                        Barcode_No LIKE @key OR 
+                        ser LIKE @key";
+                }
+
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+
+                if (!string.IsNullOrEmpty(keyword))
+                {
+                    da.SelectCommand.Parameters.AddWithValue("@key", "%" + keyword + "%");
+                }
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                gvProducts.DataSource = dt;
+                gvProducts.DataBind();
+            }
+        }
+        protected void txtQty_TextChanged(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            GridViewRow row = (GridViewRow)txt.NamingContainer;
+
+            int productId = Convert.ToInt32(gvCart.DataKeys[row.RowIndex].Value);
+
+            decimal qty;
+            if (!decimal.TryParse(txt.Text, out qty))
+                qty = 1;
+
+            if (qty <= 0)
+                qty = 1;
+
+            DataTable cart = (DataTable)Session["Cart"];
+
+            DataRow[] rows = cart.Select("ProductId=" + productId);
+
+            if (rows.Length > 0)
+            {
+                decimal price = Convert.ToDecimal(rows[0]["Price"]);
+
+                rows[0]["Qty"] = qty;
+                rows[0]["Total"] = qty * price;
+            }
+
+            Session["Cart"] = cart;
+            BindCart();
+        }
+
+        protected void gvProducts_PreRender(object sender, EventArgs e)
+        {
+            if (gvProducts.Rows.Count > 0)
+            {
+                gvProducts.UseAccessibleHeader = true;
+                gvProducts.HeaderRow.TableSection = TableRowSection.TableHeader;
+            }
         }
         protected void gvProducts_RowCommand(object sender, GridViewCommandEventArgs e)
         {
             if (e.CommandName == "AddToCart")
             {
                 int productId = Convert.ToInt32(e.CommandArgument);
-
-                System.Diagnostics.Debug.WriteLine("AddToCart clicked: " + productId);
-
-                // TODO: load product and add to cart
+                AddToCart(productId);
             }
         }
+
+        private void AddToCart(int productId)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Product_TBL WHERE ser=@id", con);
+                cmd.Parameters.AddWithValue("@id", productId);
+
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    DataTable cart = (DataTable)Session["Cart"];
+
+                    string name = dr["Product_Name"].ToString();
+                    decimal price = Convert.ToDecimal(dr["Unit_Price"]);
+
+                    DataRow[] existing = cart.Select("ProductId=" + productId);
+
+                    if (existing.Length > 0)
+                    {
+                        existing[0]["Qty"] = Convert.ToInt32(existing[0]["Qty"]) + 1;
+                        existing[0]["Total"] = Convert.ToInt32(existing[0]["Qty"]) * price;
+                    }
+                    else
+                    {
+                        DataRow row = cart.NewRow();
+                        row["ProductId"] = productId;
+                        row["Product_Name"] = name;
+                        row["Qty"] = 1;
+                        row["Price"] = price;
+                        row["Total"] = price;
+
+                        cart.Rows.Add(row);
+                    }
+
+                    Session["Cart"] = cart;
+                    BindCart();
+                }
+            }
+        }
+
+        private void BindCart()
+        {
+            DataTable cart = (DataTable)Session["Cart"];
+            gvCart.DataSource = cart;
+            gvCart.DataBind();
+
+            CalculateTotals();
+        }
+        protected void gvCart_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "RemoveItem")
+            {
+                int productId = Convert.ToInt32(e.CommandArgument);
+
+                DataTable cart = (DataTable)Session["Cart"];
+
+                DataRow[] rows = cart.Select("ProductId=" + productId);
+
+                foreach (DataRow row in rows)
+                {
+                    cart.Rows.Remove(row);
+                }
+
+                Session["Cart"] = cart;
+                BindCart();
+            }
+        }
+        private void CalculateTotals()
+        {
+            DataTable cart = (DataTable)Session["Cart"];
+            decimal subtotal = 0;
+
+            foreach (DataRow row in cart.Rows)
+            {
+                subtotal += Convert.ToDecimal(row["Total"]);
+            }
+
+            lblSubtotal.Text = subtotal.ToString("0.00");
+
+            decimal discount = 0;
+            decimal.TryParse(txtDiscount.Text, out discount);
+
+            decimal grand = subtotal - discount;
+            lblGrandTotal.Text = grand.ToString("0.00");
+        }
+
+        protected void txtDiscount_TextChanged(object sender, EventArgs e)
+        {
+            CalculateTotals();
+        }
+
         protected void btnCheckout_Click(object sender, EventArgs e)
         {
-            // test first
-            System.Diagnostics.Debug.WriteLine("Checkout clicked");
+            DataTable cart = (DataTable)Session["Cart"];
 
-            // TODO:
-            // 1. Save order to database
-            // 2. Deduct stock
-            // 3. Clear cart (Session)
+            if (cart.Rows.Count == 0) return;
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                SqlTransaction trans = con.BeginTransaction();
+
+                try
+                {
+                    foreach (DataRow row in cart.Rows)
+                    {
+                        // Insert Order Item
+                        SqlCommand cmd = new SqlCommand(
+                            "INSERT INTO Order_TBL(ProductId, Qty, Price) VALUES(@pid,@qty,@price)",
+                            con, trans);
+
+                        cmd.Parameters.AddWithValue("@pid", row["ProductId"]);
+                        cmd.Parameters.AddWithValue("@qty", row["Qty"]);
+                        cmd.Parameters.AddWithValue("@price", row["Price"]);
+                        cmd.ExecuteNonQuery();
+
+                        // Update Stock
+                        SqlCommand stockCmd = new SqlCommand(
+                            "UPDATE Product_TBL SET Stock = Stock - @qty WHERE ser=@id",
+                            con, trans);
+
+                        stockCmd.Parameters.AddWithValue("@qty", row["Qty"]);
+                        stockCmd.Parameters.AddWithValue("@id", row["ProductId"]);
+                        stockCmd.ExecuteNonQuery();
+                    }
+
+                    trans.Commit();
+
+                    Session["Cart"] = null;
+                    InitCart();
+                    BindCart();
+
+                    lblSubtotal.Text = "0.00";
+                    lblGrandTotal.Text = "0.00";
+                }
+                catch
+                {
+                    trans.Rollback();
+                }
+            }
         }
     }
 }
